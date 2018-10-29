c17f5ab186921ca156e20f0ab8272b24e868410b
Víctor <39667448+VGzsysadm@users.noreply.github.com>
2018-10-29 18:21:33 +0100

    Update configure.php
    prepared statement

--- public/configure.php
+++ public/configure.php
@@ -3,18 +3,25 @@ $servername = "localhost";
 $username = "username";
 $password = "password";
 $dbname = "dbname";
-
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "INSERT INTO app_users VALUES ( '1', 'admin', '$2y$13\$i6B17tQ/nX6J2.yg7z/St.Z6TKOyMOXw0x6KTygtHDCnWhouexAcy', 'admin@admin.com', '1', '1', 'a:1:{i:0;s:10:\"ROLE_ADMIN\";}' ); ";
-    $conn->exec($sql);
+    $stmt = $db->prepare("INSERT INTO posts ( id, username, password, email, isactive, termsAccepted, roles, lang ) VALUES ( NULL, ?, ?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("ssssssss", $id, $username, $password, $email, $isactive, $termsAccepted, $roles, $lang  );
+    $id = '1';
+    $username = 'admin';
+    $password = '$2y$13\$i6B17tQ/nX6J2.yg7z/St.Z6TKOyMOXw0x6KTygtHDCnWhouexAcy';
+    $email = 'admin@admin.local';
+    $isactive = '1';
+    $termsAccepted = '1';
+    $roles = ''a:1:{i:0;s:10:\"ROLE_ADMIN\";}'';
+    $lang = 'en_EN';
+    $stmt->execute();
     echo "The user admin has been created with the password admin, now you can log in.";
 }
 catch(PDOException $e)
 {
     echo "You cannot use this query again." . "<br>" . "If you used already this config file, please remove it urgently from the public folder.";
 }
-
 $conn = null;
 ?>
