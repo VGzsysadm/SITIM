0a55526b37d318709e27a439bc94f0f0ae0357a9
Víctor <39667448+VGzsysadm@users.noreply.github.com>
2018-10-30 10:15:20 +0100

    Update configure.php
    prepared statement.

--- public/configure.php
+++ public/configure.php
@@ -1,27 +1,34 @@
 <?php
 $servername = "localhost";
-$username = "username";
-$password = "password";
+$usr = "usr";
+$pwd = "pwd";
 $dbname = "dbname";
+
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+	$id = "1";
+	$username = "admin";
+	$password = "$2y$13\$i6B17tQ/nX6J2.yg7z/St.Z6TKOyMOXw0x6KTygtHDCnWhouexAcy";
+	$email = "admin@admin.local";
+	$is_active = "1";
+	$terms_accepted = "1";
+	$roles = "a:1:{i:0;s:10:\"ROLE_ADMIN\";}";
+	$lang = "en_EN";
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $usr, $pwd);
+    // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $stmt = $db->prepare("INSERT INTO app_users ( id, username, password, email, isactive, termsAccepted, roles, lang ) VALUES ( NULL, ?, ?, ?, ?, ?, ?, ?)");
-    $stmt->bind_param("ssssssss", $id, $username, $password, $email, $isactive, $termsAccepted, $roles, $lang  );
-    $id = '1';
-    $username = 'admin';
-    $password = '$2y$13\$i6B17tQ/nX6J2.yg7z/St.Z6TKOyMOXw0x6KTygtHDCnWhouexAcy';
-    $email = 'admin@admin.local';
-    $isactive = '1';
-    $termsAccepted = '1';
-    $roles = ''a:1:{i:0;s:10:\"ROLE_ADMIN\";}'';
-    $lang = 'en_EN';
-    $stmt->execute();
-    echo "The user admin has been created with the password admin, now you can log in.";
-}
+    $stmt = $conn->prepare('INSERT INTO app_users (id, username, password, email, is_active, terms_accepted, roles, lang) VALUES (:id, :username, :password, :email, :is_active, :terms_accepted, :roles, :lang)');
+	$stmt-> bindValue(":id",$id,PDO::PARAM_INT);
+	$stmt-> bindValue(":username",$username,PDO::PARAM_STR);
+	$stmt-> bindValue(":password",$password,PDO::PARAM_STR);
+	$stmt-> bindValue(":email",$email,PDO::PARAM_STR);
+	$stmt-> bindValue(":is_active",$is_active,PDO::PARAM_BOOL);
+	$stmt-> bindValue(":terms_accepted",$terms_accepted,PDO::PARAM_BOOL);
+	$stmt-> bindValue(":roles",$roles,PDO::PARAM_STR);
+	$stmt-> bindValue(":lang",$lang,PDO::PARAM_BOOL);
+	$stmt->execute();
+    }
 catch(PDOException $e)
-{
-    echo "You cannot use this query again." . "<br>" . "If you used already this config file, please remove it urgently from the public folder.";
-}
-$conn = null;
-?>
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+?> 
